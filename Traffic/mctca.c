b22295ccdbfef8c907247fb72be55ee2893da9f1
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-06-18 12:28:25 -0500

    Implementing multicell model

--- Traffic/mctca.c
+++ Traffic/mctca.c
@@ -25,117 +25,87 @@
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 
-double rnd(){
-    clock_t t;
-    t=clock();
-    double tc;
-    tc = (((float)t)/CLOCKS_PER_SEC);
-    long double X = fmod ((PI*t)+(PI/tc),(PI/tc));/*Seed*/
-    double M = 2147483648,n;
-    int a = 1103515245,c=12345,i;
-    for(i=1;i<5;i++){
-        X = fmod ((a*X+c),M);// Linear congruence
-        n = (X/(M-1));
-    }  
-
-    return n;
-}
 
 struct Cell
 {
     int type;
     int velocity;
+    bool evaluated;
 
 };
 
-void initialize( struct Cell ** matrix ){
-    for (int i = 0; i < rowDim; ++i){
-        for (int j = 0; j < colDim; ++j){
-            matrix[i][j] = (struct Cell) { .type = Freeway, .velocity = 0 };
-        }
-    } 
-}
+// double rnd(){
+//     clock_t t;
+//     t=clock();
+//     double tc;
+//     tc = (((float)t)/CLOCKS_PER_SEC);
+//     long double X = fmod ((PI*t)+(PI/tc),(PI/tc));/*Seed*/
+//     double M = 2147483648,n;
+//     int a = 1103515245,c=12345,i;
+//     for(i=1;i<5;i++){
+//         X = fmod ((a*X+c),M);// Linear congruence
+//         n = (X/(M-1));
+//     }  
 
-void newCar(struct Cell ** matrix, int row, int col){
-    if(row + 1 < rowDim && col + 1 < colDim){
-        struct Cell car = { .type = Car, .velocity = rnd() * Vmax + 1 };
-        matrix[row][col] = car;
-        matrix[row][col + 1] = car;
-        matrix[row + 1][col] = car;
-        matrix[row + 1][col + 1] = car;
-    }
-}
+//     return n;
+// }
 
-void newMotorbike(struct Cell ** matrix, int row, int col){
-    if(row + 1 < rowDim && col + 1 < colDim){
-        struct Cell motorbike = { .type = Motorbike, .velocity = rnd() * Vmax + 1 };
-        matrix[row][col] = motorbike;
-    }
-}
+// void initialize( struct Cell ** matrix ){
+//     for (int i = 0; i < rowDim; ++i){
+//         for (int j = 0; j < colDim; ++j){
+//             matrix[i][j] = (struct Cell) { .type = Freeway, .velocity = 0 };
+//         }
+//     } 
+// }
 
-void fillCellularSpace( struct Cell ** matrix ){
-    for (int i = 0; i < rowDim; ++i){
-        for (int j = 0; j < colDim; ++j){
-            if(rnd() < VehicheProb && (matrix[i][j]).type == Freeway){
-                if(rnd() < CarProb)
-                    newCar(matrix,i,j);
-                else
-                    newMotorbike(matrix,i,j);
-            }
-        }
-    }
-}
+// void newCar(struct Cell ** matrix, int row, int col){
+//     if(row + 1 < rowDim && col + 1 < colDim){
+//         struct Cell car = { .type = Car, .velocity = rnd() * Vmax + 1 };
+//         matrix[row][col] = car;
+//         matrix[row][col + 1] = car;
+//         matrix[row + 1][col] = car;
+//         matrix[row + 1][col + 1] = car;
+//     }
+// }
 
+// void newMotorbike(struct Cell ** matrix, int row, int col){
+//     if(row + 1 < rowDim && col + 1 < colDim){
+//         struct Cell motorbike = { .type = Motorbike, .velocity = rnd() * Vmax + 1 };
+//         matrix[row][col] = motorbike;
+//     }
+// }
 
-
-// struct Neighborhood
-// {
-//     int up;
-//     int center;
-//     int down;
-//     int left;
-//     int left_up;
-//     int left_down;
-//     int right;
-//     int right_up;
-//     int right_down;
-
-// };
-
-// struct Neighborhood neighborhoodOf( int ** matrix, int row, int col){
-//     struct Neighborhood neighborhood;
-//     neighborhood.up = matrix[ MOD(row - 1,rowDim) ][ col ];
-//     neighborhood.down = matrix[ MOD(row + 1,rowDim) ][ col ];
-    
-//     neighborhood.left = matrix[ row ][ MOD(col - 1,colDim) ];
-//     neighborhood.left_up = matrix[ MOD(row - 1,rowDim) ][ MOD(col - 1,colDim) ];
-//     neighborhood.left_down = matrix[ MOD(row + 1,rowDim) ][ MOD(col - 1,colDim) ];
-    
-//     neighborhood.right = matrix[ row ][ MOD(col + 1,colDim) ];
-//     neighborhood.right_up = matrix[ MOD(row - 1,rowDim) ][ MOD(col + 1,colDim) ];
-//     neighborhood.right_down = matrix[ MOD(row + 1,rowDim) ][ MOD(col + 1,colDim) ];
-
-//     return neighborhood;
+// void fillCellularSpace( struct Cell ** matrix ){
+//     for (int i = 0; i < rowDim; ++i){
+//         for (int j = 0; j < colDim; ++j){
+//             if(rnd() < VehicheProb && (matrix[i][j]).type == Freeway){
+//                 if(rnd() < CarProb)
+//                     newCar(matrix,i,j);
+//                 else
+//                     newMotorbike(matrix,i,j);
+//             }
+//         }
+//     }
 // }
 
 
-void nextState( struct Cell ** in, struct Cell ** out, int row, int col ){
+// void nextState( struct Cell ** in, struct Cell ** out, int row, int col ){
 
-    struct Cell cell = in[row][col];
+//     struct Cell cell = in[row][col];
 
-    if(cell.type != Freeway){
-        // Step 1: Acceleration
-        velocity = MIN(velocity + 1,Vmax);
-        // Step 2: Safe distance
-        int freecells = 0;
-        for(int i = row; in[ MOD( i - 1 , rowDim ) ][col] == Freeway; i = MOD( i - 1, rowDim )) ++freecells;
-        velocity = MIN(velocity,freecells - 1);
-        // Step 3: Randomness deacceleration
-        velocity = ( rnd() < DeaccelProb ) ? MAX(0,velocity - 1) : velocity;
-        // Step 4: New position
-        out[ MOD( row - velocity, rowDim ) ][col] = velocity;
-    }
-}
+//     if(cell.type != Freeway){
+//         // Step 1: Acceleration
+//         velocity = MIN(velocity + 1,Vmax);
+//         // Step 2: Safe distance
+//         int freecells = 0;
+//         for(int i = row; in[ MOD( i - 1 , rowDim ) ][col] == Freeway; i = MOD( i - 1, rowDim )) ++freecells;
+//         velocity = MIN(velocity,freecells - 1);
+//         // Step 3: Randomness deacceleration
+//         velocity = ( rnd() < DeaccelProb ) ? MAX(0,velocity - 1) : velocity;
+//         // Step 4: New position
+//         out[ MOD( row - velocity, rowDim ) ][col] = velocity;
+//     }
+// }
 
 // void step( int ** in, int ** out)
 // {
@@ -147,25 +117,25 @@ void nextState( struct Cell ** in, struct Cell ** out, int row, int col ){
 //     }
 // }
 
-void see(struct Cell ** matrix){
-    printf("\n");
-    int i = 0;
-    for (i=0; i<rowDim; ++i){
-        int j = 0;
-        for (j=0; j<colDim; ++j){
-            if(matrix[i][j].type == Freeway)
-                printf("[ ] ");
-            else{
-                if(matrix[i][j].type == Car)
-                    printf("[*] ");
-                else
-                    printf("[^] ");
-            }
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
+// void see(struct Cell ** matrix){
+//     printf("\n");
+//     int i = 0;
+//     for (i=0; i<rowDim; ++i){
+//         int j = 0;
+//         for (j=0; j<colDim; ++j){
+//             if(matrix[i][j].type == Freeway)
+//                 printf("[ ] ");
+//             else{
+//                 if(matrix[i][j].type == Car)
+//                     printf("[*] ");
+//                 else
+//                     printf("[^] ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     printf("\n");
+// }
 
 // void evolve( int ** in, int ** out){
 //     int i = 0;
@@ -186,8 +156,8 @@ void see(struct Cell ** matrix){
 int main(int argc, char const **argv)
 {
     /* Celular space initialization */
-    struct Cell ** in = ( struct Cell **) malloc(rowDim*sizeof( struct Cell * ));
-    struct Cell ** out = ( struct Cell **) malloc(rowDim*sizeof( struct Cell * ));
+    struct Cell *** in = ( struct Cell *** ) malloc(rowDim*sizeof( struct Cell ** ));
+    struct Cell *** out = ( struct Cell *** ) malloc(rowDim*sizeof( struct Cell ** ));
 
     /* Allocation error checking */
     if( in == NULL || out == NULL ){
@@ -198,20 +168,34 @@ int main(int argc, char const **argv)
     /* Rows allocation */
     for (int i=0; i<rowDim; ++i){ 
         /* for each row we allocate colDim integers */
-        in[i] = ( struct Cell *) malloc(colDim*sizeof(struct Cell));
-        out[i] = ( struct Cell *) malloc(colDim*sizeof(struct Cell));
+        in[i] = ( struct Cell ** ) malloc(colDim*sizeof( struct Cell * ));
+        out[i] = ( struct Cell ** ) malloc(colDim*sizeof( struct Cell * ));
     }
 
-    initialize(in);
-    fillCellularSpace(in);
-    see(in);
-
-    /* Running simulation */
-    // evolve(in,out);
+    for (int i=0; i<rowDim; ++i){
+        for(int j=0; j<colDim; ++j){
+            in[i][j] = ( struct Cell * ) malloc(sizeof( struct Cell ));
+            out[i][j] = ( struct Cell * ) malloc(sizeof( struct Cell ));
+            
+            struct Cell cellin = (struct Cell) { .type = Freeway, .velocity = 0 };
+            in[i][j] = NULL;
+            
+            struct Cell cellout = (struct Cell) { .type = Freeway, .velocity = 0 };
+            out[i][j] = NULL;
+        }
+    }
 
     /* -- Releasing resources -- */
+    for (int i=0; i<rowDim; ++i){
+        for(int j=0; j<colDim; ++j){
+            free(in[i][j]);
+            free(out[i][j]);
+        }
+    }
+
     for (int i=0; i<rowDim; ++i) free(in[i]);
     free(in);
+
     for (int i=0; i<rowDim; ++i) free(out[i]);
     free(out);
 
